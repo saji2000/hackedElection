336c389070b677d1395aba72595dd810d4a22c79
Sooraj-beep <55115712+Sooraj-beep@users.noreply.github.com>
2020-01-18 15:31:41 -0700

    Add files via upload

---
+++ block_chain.py
@@ -0,0 +1,18 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain[]
+        self.current_votes[]
+
+    def new_block(self):
+        pass
+
+    def new_vote(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
