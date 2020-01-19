1b4f9dfc0c82ecfed97af6e642994d9a2f35c6be
usamaa18 <52779223+usamaa18@users.noreply.github.com>
2020-01-18 17:16:35 -0700

    Add files via upload

--- block_chain.py
+++ block_chain.py
@@ -3,27 +3,103 @@ import json
 from time import time
 from urllib.parse import urlparse
 from uuid import uuid4
-
 import requests
 from flask import Flask, jsonify, request
+
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_votes = []
-        
-    def new_block(self):
-        pass
 
-    def new_vote(self):
-        pass
+        self.new_block(proof = 100, previous_hash = 1)
+
+    def new_vote(self, voter, candidate):
+        self.current_votes.append({
+            'voter': voter,
+            'candidate': candidate,
+
+        })
+
+        return self.last_block['index'] + 1
+
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_votes,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_votes = []
+
+        self.chain.append(block)
+        return block
+
+
 
     @staticmethod
     def hash(block):
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
 
-print("hello")
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, Wo!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
